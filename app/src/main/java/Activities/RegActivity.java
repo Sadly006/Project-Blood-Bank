3269f065a1a84a56af99a33770d409b8783d8cbb
Sadly006 <taosifsadlyts7@gmail.com>
2021-07-22 23:51:04 +0600

    checking if registration page inputs are ok

--- app/src/main/java/Activities/RegActivity.java
+++ app/src/main/java/Activities/RegActivity.java
@@ -2,23 +2,16 @@ package Activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
-import android.preference.PreferenceManager;
 
-import java.lang.reflect.Method;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
-import javax.xml.transform.ErrorListener;
 
 import com.example.bloodbank.R;
 
@@ -45,11 +38,48 @@ public class RegActivity extends AppCompatActivity {
                 group = groupEt.getText().toString();
                 contact = contactEt.getText().toString();
                 password = passwordEt.getText().toString();
+                if (isValid(name, location, group, password, contact)) {
 
                 }
-            });
-
-        }
-
+            }
+        });
 
     }
+
+    private void showMessage(String msg) {
+        Toast.makeText(this, msg, Toast.LENGTH_SHORT).show();
+    }
+
+
+    private boolean isValid(String name, String location, String group, String password,
+                            String contact) {
+        List<String> validBloodGroups = new ArrayList<>();
+        validBloodGroups.add("A+");
+        validBloodGroups.add("A-");
+        validBloodGroups.add("B+");
+        validBloodGroups.add("B-");
+        validBloodGroups.add("AB+");
+        validBloodGroups.add("AB-");
+        validBloodGroups.add("O+");
+        validBloodGroups.add("O-");
+        if (name.isEmpty()) {
+            showMessage("Name is empty");
+            return false;
+        } else if (location.isEmpty()) {
+            showMessage("Location is required");
+            return false;
+        } else if (!validBloodGroups.contains(group)) {
+            showMessage("Blood group invalid choose from " + validBloodGroups);
+            return false;
+        } else if (contact.length() != 10) {
+            showMessage("Invalid mobile number, number should be of 10 digits");
+            return false;
+        } else if (password.isEmpty()) {
+            showMessage("Password is required");
+            return false;
+        }
+
+        return true;
+
+    }
+}
